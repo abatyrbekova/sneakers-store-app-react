2c32018521d752b5559a726539bcfe506ce1150f
abatyrbekova <batyrbekova592@gmail.com>
2022-08-18 01:27:16 +0200

    create and style 'Bag' page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,64 @@ import React from "react";
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="d-flex justify-between mb-30">
+            Bag
+            <img className="cu-p" src="/img/btn-remove.svg" alt="remove" />
+          </h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/2.jpg)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Men Nike Air Max 270</p>
+                <b>129,99 €</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/4.jpg)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Puma X Aka Boku Future Rider</p>
+                <b>89,99 €</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Summary:</span>
+                <div></div>
+                <b>219,98 €</b>
+              </li>
+              <li>
+                <span>Taxes 5%:</span>
+                <div></div>
+                <b>11 €</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Checkout <img src="/img/arrow.svg" alt="arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" alt="logo" />
@@ -17,7 +75,7 @@ function App() {
             <span>12,99 €</span>
           </li>
           <li>
-            <img width={18} height={18} src="/img/user.svg" alt="cart" />
+            <img width={18} height={18} src="/img/user.svg" alt="user" />
           </li>
         </ul>
       </header>
@@ -26,13 +84,16 @@ function App() {
         <div className="d-flex align-center justify-between mb-40">
           <h1>All sneakers</h1>
           <div className="search-block d-flex">
-          <img src="/img/search.svg" alt="search" />
-          <input placeholder="Search..." />
+            <img src="/img/search.svg" alt="search" />
+            <input placeholder="Search..." />
           </div>
         </div>
         <div className="d-flex">
-
           <div className="card">
+            <div className="favorite">
+              <img src="/img/unliked.svg" alt="unliked" />
+            </div>
+
             <img
               width={133}
               height={112}
@@ -221,7 +282,6 @@ function App() {
               </button>
             </div>
           </div>
-
         </div>
       </div>
     </div>
