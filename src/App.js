ee52bce9ef4c084730ed4884d62289cabf7f8540
abatyrbekova <batyrbekova592@gmail.com>
2022-10-11 22:23:48 +0200

    fetch, store data in API

--- src/App.js
+++ src/App.js
@@ -5,57 +5,19 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
-const arr = [
-  {
-    title: "Men Nike Blazer Mid Suede",
-    price: 129.99,
-    imageURL: "/img/sneakers/1.jpg",
-  },
-  {
-    title: "Men Nike Air Max 270",
-    price: 129.99,
-    imageURL: "/img/sneakers/2.jpg",
-  },
-  {
-    title: "Men Nike Blazer Mid Suede",
-    price: 84.99,
-    imageURL: "/img/sneakers/3.jpg",
-  },
-  {
-    title: "Puma X Aka Boku Future Rider",
-    price: 89.99,
-    imageURL: "/img/sneakers/4.jpg",
-  },
-  {
-    title: "Men Under Armour Curry 8",
-    price: 151.99,
-    imageURL: "/img/sneakers/5.jpg",
-  },
-  { title: "Men Nike Kyrie 7", price: 112.99, imageURL: "/img/sneakers/6.jpg" },
-  {
-    title: "Men Jordan Air Jordan 11",
-    price: 107.99,
-    imageURL: "/img/sneakers/7.jpg",
-  },
-  {
-    title: "Men Nike LeBron XVIII",
-    price: 164.99,
-    imageURL: "/img/sneakers/8.jpg",
-  },
-  {
-    title: "Men Nike Lebron XVIII Low",
-    price: 139.99,
-    imageURL: "/img/sneakers/9.jpg",
-  },
-  {
-    title: "Men Nike Kyrie Flytrap IV",
-    price: 112.99,
-    imageURL: "/img/sneakers/10.jpg",
-  },
-];
-
 function App() {
+  const [items, setItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://63457202dcae733e8ff2cb36.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
 
   return (
     <div className="wrapper clear">
@@ -70,8 +32,8 @@ function App() {
             <input placeholder="Search..." />
           </div>
         </div>
-        <div className="d-flex">
-          {arr.map((obj) => (
+        <div className="d-flex flex-wrap">
+          {items.map((obj) => (
             <Card
               title={obj.title}
               price={obj.price}
