059259acf52895064372498ccbb07f848d810450
“erikayanez“ <erikajoam@gmail.com>
2022-10-19 13:24:33 +0200

    First commit of the project

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+  )
+  .then(() => {
+    console.log("Database connected! 😃");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    console.log("🤨");
+  });
+
+  app.listen(3000, (re, res) => {
+    console.log("Server running on port 3000");
+  });
